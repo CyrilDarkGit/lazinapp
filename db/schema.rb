ffeae53f5974912762cb4df6f4eee0ce4c7eed20
Marine Ramillon <marine.ramillon@gmail.com>
2022-05-25 12:41:34 +0200

    adding map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_24_151325) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_25_093242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_24_151325) do
     t.datetime "updated_at", null: false
     t.string "currency"
     t.string "category"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
