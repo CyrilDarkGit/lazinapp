ee468121b89a1aaaf8eb4c8aa0b14caae76c0955
Marine Ramillon <marine.ramillon@gmail.com>
2022-05-26 15:15:37 +0200

    dashboard try

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,14 @@
 class DashboardsController < ApplicationController
+
+
+
   def show
     @user = current_user
+    # @test = Booking.all.where(offer: {user: @user})
+    # @im_seller = Booking.joins(:offer).joins(:user).where(offer: {user: @user})
     @upcoming_bookings = @user.bookings.select { |booking| booking.date >= Date.today}
     @past_bookings = @user.bookings.select { |booking| booking.date < Date.today}
-    @pending_booking = @user.bookings.select { |booking| booking.status = "pending" }
+    @pending_booking = @user.bookings.select { |booking| booking.status == "pending" }
+
   end
 end
