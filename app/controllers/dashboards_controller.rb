660182683e98a9b889a8e917dc200e7522ca1842
Marine Ramillon <marine.ramillon@gmail.com>
2022-05-26 16:17:00 +0200

    changing dashboard back

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,11 +4,15 @@ class DashboardsController < ApplicationController
 
   def show
     @user = current_user
-    # @test = Booking.all.where(offer: {user: @user})
-    # @im_seller = Booking.joins(:offer).joins(:user).where(offer: {user: @user})
+    @rented_offers = @user.rented_offers #marche seulement car on a crée rented_offers in user model
+    # ou on aurait pu ecrire @rented_offers = Booking.joins(:offer).joins(:user).where(offer: {user: @user})
+    # ou encore @bookings_requests = []
+    # @user.offers.each { |offer| @bookings_requests << offer.bookings}
+
     @upcoming_bookings = @user.bookings.select { |booking| booking.date >= Date.today}
     @past_bookings = @user.bookings.select { |booking| booking.date < Date.today}
     @pending_booking = @user.bookings.select { |booking| booking.status == "pending" }
+    @validate_booking = @user.bookings.select { |booking| booking.status == "validate" }
 
   end
 end
