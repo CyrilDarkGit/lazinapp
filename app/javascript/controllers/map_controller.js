4104a7211185354445c302ea3d799a3abf851983
Marine Ramillon <marine.ramillon@gmail.com>
2022-05-26 16:54:37 +0200

    zoom map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -31,8 +31,8 @@ export default class extends Controller {
 
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 50, duration: 0 })
+    this.markersValue.forEach(marker => bounds.extend([ marker.longitude, marker.latitude ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 
 }
